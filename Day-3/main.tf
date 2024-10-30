f3cf984fe411a82110fcbc5d180f4651da0470d0
Jayalakshmi-i <jayalakshmi-i@gmail.com>
2024-10-30 21:17:45 +0530

    update

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
+  source = "./modules/ec2_instance"  # from where it has to fetch the module from it can be in any github location/account
   ami_value = "ami-053b0d53c279acc90" # replace this
   instance_type_value = "t2.micro"
   subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
